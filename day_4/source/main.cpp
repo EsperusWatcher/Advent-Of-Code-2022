437451000f465aba71683130485382911a268570
Watcher <ivndonat@yandex.ru>
2023-01-02 10:48:56 +0300

    day 4 part 1

---
+++ day_4/source/main.cpp
@@ -0,0 +1,64 @@
+#include "../include/include.h"
+
+// the file contains pairs of ranges
+// find the amount of ranges that contain their pair in full
+
+
+int CheckForNesting(int n1, int n2, int n3, int n4)
+{
+    if (n1 <= n3)
+        if (n2 >= n4)
+            return 1;
+
+    if (n3 <= n1)
+        if (n4 >= n2)
+            return 1;
+
+    return 0;
+}
+
+// Format is N1-N2,N3-N4
+int TranslateLine(std::string line)
+{
+    int n1, n2, n3, n4;
+
+    n1 = stoi(line.substr(0, line.find("-")));
+
+    line.erase(0, line.find("-") + 1);
+
+    n2 = stoi(line.substr(0, line.find(",")));
+
+    line.erase(0, line.find(",") + 1);
+
+    n3 = stoi(line.substr(0, line.find("-")));
+ 
+    line.erase(0, line.find("-") + 1);
+ 
+    n4 = stoi(line);
+
+
+    return CheckForNesting(n1, n2, n3, n4);
+}
+
+int main()
+{
+    std::fstream file_hook;
+
+    file_hook.open("puzzle_input.txt", std::ios::in);
+
+    std::string file_line;
+
+    int total_full_covers = 0;
+
+    do
+    {
+        getline(file_hook, file_line);
+
+        total_full_covers += TranslateLine(file_line);
+
+    } while (!file_hook.eof());
+    
+    std::cout << "Total full covers: " << total_full_covers << "\n";
+
+    return 0;
+}
